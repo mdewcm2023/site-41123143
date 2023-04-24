e7d9ff2a4e7eb843313ef2ee5e963fb9df919ff7
41123143 <41123143@med.tw>
2023-04-24 14:58:22 +0800

    增加倉儲

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲:', 'tags': '', 'url': 'About.html'}]};
